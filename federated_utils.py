39bebbde2f479740a5874d3429b2e816fe339614
zhanglei1949 <zhanglei1949@sjtu.edu.cn>
2019-10-24 14:06:28 +0800

    use gpu to accerlerate federated computation

--- federated_utils.py
+++ federated_utils.py
@@ -36,3 +36,41 @@ def transNumpyGrad2Cuda(grad, shape_list):
         res.append(tmp.float().cuda())
         ind += listMulti(shape)
     return res
+
+############ gpu version
+def getLenOfGradientVectorCuda(current_grad):
+    #expect a list consists of numpy arrays
+    n = 0
+    for arr in current_grad:
+        #print(list(arr.view(-1,).shape)[0])
+        n += list(arr.view(-1,).shape)[0]
+    return n
+def getShapeListCuda(current_grad):
+    # return the list of shapes of grad vectors, for recover
+    res = []
+    for arr in current_grad:
+        res.append(arr.shape)
+    return res
+def transListOfArraysToArraysCuda(current_grad, n):
+    # expect a list of arrays, return a squeezed cuda array, n is the total length
+    res = torch.zeros(n).cuda()
+    ind = 0
+    for arr in current_grad:
+        arr = arr.view(-1,)
+        res[ind:ind+arr.shape[0]] = arr
+        ind+=arr.shape[0]
+    return res
+def listMultiCuda(l):
+    res = 1
+    for ele in l:
+        res *= ele
+    return res
+def transCudaArrayWithShapeList(grad, shape_list):
+    res = []
+    ind = 0
+    for shape in shape_list:
+        tmp = grad[ind:ind+listMulti(shape)]
+        tmp = tmp.view(shape)
+        res.append(tmp.float())
+        ind += listMulti(shape)
+    return res
