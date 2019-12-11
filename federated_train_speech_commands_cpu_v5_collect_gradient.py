2a9b8d95ffe6154dd9856d534dbca692db66eee6
zhanglei1949 <zhanglei1949@sjtu.edu.cn>
2019-12-11 09:15:06 +0000

    add gradient analysis

---
+++ federated_train_speech_commands_cpu_v5_collect_gradient.py
@@ -0,0 +1,267 @@
+# Dump index matrix and original gradient and masked gradient into dataframes.
+
+import argparse
+import time
+import scipy, math
+from scipy.linalg import null_space
+from tqdm import *
+
+import torch
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import WeightedRandomSampler
+
+import torchvision
+from torchvision.transforms import *
+
+from tensorboardX import SummaryWriter
+
+import models
+from datasets import *
+from transforms import *
+from mixup import *
+from federated_utils_cpu_v3 import Federated
+
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--gradient-res-path", type = str, default = './analysis/', help = 'root path for several matrix')
+parser.add_argument("--clients", type = int, default = 5, help= 'number of clients')
+parser.add_argument("--num-threads", type = int, default = 10, help= 'number of threads')
+parser.add_argument("--matrix-size", type = int, default = 1000, help= 'size of randomization matrix')
+parser.add_argument("--train-dataset", type=str, default='datasets/speech_commands/train', help='path of train dataset')
+parser.add_argument("--valid-dataset", type=str, default='datasets/speech_commands/valid', help='path of validation dataset')
+parser.add_argument("--background-noise", type=str, default='datasets/speech_commands/train/_background_noise_', help='path of background noise')
+parser.add_argument("--comment", type=str, default='', help='comment in tensorboard title')
+parser.add_argument("--batch-size", type=int, default=128, help='batch size')
+parser.add_argument("--dataload-workers-nums", type=int, default=6, help='number of workers for dataloader')
+parser.add_argument("--weight-decay", type=float, default=1e-2, help='weight decay')
+parser.add_argument("--optim", choices=['sgd', 'adam'], default='sgd', help='choices of optimization algorithms')
+parser.add_argument("--learning-rate", type=float, default=1e-4, help='learning rate for optimization')
+parser.add_argument("--lr-scheduler", choices=['plateau', 'step'], default='plateau', help='method to adjust learning rate')
+parser.add_argument("--lr-scheduler-patience", type=int, default=5, help='lr scheduler plateau: Number of epochs with no improvement after which learning rate will be reduced')
+parser.add_argument("--lr-scheduler-step-size", type=int, default=50, help='lr scheduler step: number of epochs of learning rate decay.')
+parser.add_argument("--lr-scheduler-gamma", type=float, default=0.1, help='learning rate is multiplied by the gamma to decrease it')
+parser.add_argument("--max-epochs", type=int, default=70, help='max number of epochs')
+parser.add_argument("--resume", type=str, help='checkpoint file to resume')
+parser.add_argument("--model", choices=models.available_models, default=models.available_models[0], help='model of NN')
+parser.add_argument("--input", choices=['mel32'], default='mel32', help='input of NN')
+parser.add_argument('--mixup', action='store_true', help='use mixup')
+args = parser.parse_args()
+
+use_gpu = torch.cuda.is_available()
+use_gpu = 'True'
+print('use_gpu', use_gpu)
+print('num of clients', args.clients)
+if use_gpu:
+    torch.backends.cudnn.benchmark = True
+
+n_mels = 32
+
+def build_dataset(n_mels = n_mels, train_dataset = args.train_dataset, valid_dataset = args.valid_dataset, background_noise = args.background_noise):
+    data_aug_transform = Compose([ChangeAmplitude(), ChangeSpeedAndPitchAudio(), FixAudioLength(), ToSTFT(), StretchAudioOnSTFT(), TimeshiftAudioOnSTFT(), FixSTFTDimension()])
+    bg_dataset = BackgroundNoiseDataset(background_noise, data_aug_transform)
+    add_bg_noise = AddBackgroundNoiseOnSTFT(bg_dataset)
+    train_feature_transform = Compose([ToMelSpectrogramFromSTFT(n_mels=n_mels), DeleteSTFT(), ToTensor('mel_spectrogram', 'input')])
+    train_dataset = SpeechCommandsDataset(train_dataset,
+                                Compose([LoadAudio(),
+                                         data_aug_transform,
+                                         add_bg_noise,
+                                         train_feature_transform]))
+
+    valid_feature_transform = Compose([ToMelSpectrogram(n_mels=n_mels), ToTensor('mel_spectrogram', 'input')])
+    valid_dataset = SpeechCommandsDataset(valid_dataset,
+                                Compose([LoadAudio(),
+                                         FixAudioLength(),
+                                         valid_feature_transform]))
+    return train_dataset, valid_dataset
+
+def main():
+    # 1. load dataset, train and valid
+    train_dataset, valid_dataset = build_dataset(n_mels = n_mels, train_dataset = args.train_dataset, valid_dataset = args.valid_dataset, background_noise = args.background_noise)
+    print('train ',len(train_dataset), 'val ', len(valid_dataset))
+
+    weights = train_dataset.make_weights_for_balanced_classes()
+    sampler = WeightedRandomSampler(weights, len(weights))
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, sampler=sampler,
+                              pin_memory=use_gpu, num_workers=args.dataload_workers_nums)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False,
+                              pin_memory=use_gpu, num_workers=args.dataload_workers_nums)
+    # a name used to save checkpoints etc.
+    # 2. prepare the model, checkpoint
+    full_name = '%s_%s_%s_bs%d_lr%.1e_wd%.1e' % (args.model, args.optim, args.lr_scheduler, args.batch_size, args.learning_rate, args.weight_decay)
+    if args.comment:
+        full_name = '%s_%s' % (full_name, args.comment)
+
+    model = models.create_model(model_name=args.model, num_classes=len(CLASSES), in_channels=1)
+    if use_gpu:
+        model = torch.nn.DataParallel(model).cuda()
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    if args.optim == 'sgd':
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=args.weight_decay)
+    else:
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+
+    start_timestamp = int(time.time()*1000)
+    start_epoch = 0
+    best_accuracy = 0
+    best_loss = 1e100
+    global_step = 0
+
+    if args.resume:
+        print("resuming getShapeLista checkpoint '%s'" % args.resume)
+        checkpoint = torch.load(args.resume)
+        model.load_state_dict(checkpoint['state_dict'])
+        model.float()
+        optimizer.load_state_dict(checkpoint['optimizer'])
+
+        best_accuracy = checkpoint.get('accuracy', best_accuracy)
+        best_loss = checkpoint.get('loss', best_loss)
+        start_epoch = checkpoint.get('epoch', start_epoch)
+        global_step = checkpoint.get('step', global_step)
+
+        del checkpoint  # reduce memory
+
+    if args.lr_scheduler == 'plateau':
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=args.lr_scheduler_patience, factor=args.lr_scheduler_gamma)
+    else:
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_scheduler_step_size, gamma=args.lr_scheduler_gamma, last_epoch=start_epoch-1)
+
+    def get_lr():
+        return optimizer.param_groups[0]['lr']
+
+    writer = SummaryWriter(comment=('_speech_commands_' + full_name))
+
+    #3. train and validation
+    print("training %s for Google speech commands..." % args.model)
+    since = time.time()
+    #grad_client_list = [[]] * args.clients
+    federated = Federated(args.clients, args.matrix_size, args.num_threads, args.gradient_res_path)
+    for epoch in range(start_epoch, args.max_epochs):
+        print("epoch %3d with lr=%.02e" % (epoch, get_lr()))
+        phase = 'train'
+        writer.add_scalar('%s/learning_rate' % phase,  get_lr(), epoch)
+        model.train()  # Set model to training mode
+
+        running_loss = 0.0
+        it = 0
+        correct = 0
+        total = 0
+
+        #compute for each client
+        current_client = 0
+        pbar = tqdm(train_dataloader, unit="audios", unit_scale=train_dataloader.batch_size, disable=False)
+        # for i in range(0, train_dataloader.__len__, self.batch_size):
+        #     pbar.
+        #print(pbar.total, len(train_dataloader), len(train_dataset), train_dataloader.batch_size)
+        #num_batches = len(pbar.total)
+        
+
+        for batch in pbar:
+            inputs = batch['input']
+            inputs = torch.unsqueeze(inputs, 1)
+            targets = batch['target']
+            #print(inputs.shape, targets.shape)
+            if args.mixup:
+                inputs, targets = mixup(inputs, targets, num_classes=len(CLASSES))
+
+            inputs = Variable(inputs, requires_grad=True)
+            targets = Variable(targets, requires_grad=False)
+            if use_gpu:
+                inputs = inputs.cuda()
+                targets = targets.cuda(async=True)
+
+            outputs = model(inputs)
+            if args.mixup:
+                loss = mixup_cross_entropy_loss(outputs, targets)
+            else:
+                loss = criterion(outputs, targets)
+            optimizer.zero_grad()
+            loss.backward()
+            current_client_grad = torch.zeros(1,1).cuda()
+            shape_list = []
+            for name, param in model.named_parameters():
+                if param.requires_grad:
+                    #print(name, param.grad.shape, param.grad.type())#, param.grad)
+                    #current_client_grad.append(param.grad)
+                    shape_list.append(list(param.grad.shape))
+                    current_client_grad = torch.cat((current_client_grad, param.grad.view(-1,1)), 0)
+            #break
+            current_client_grad = current_client_grad[1:,:].view(-1,)
+            #print(current_client_grad.shape)
+            if (current_client == 0):
+                federated.init(current_client_grad,shape_list)
+             
+            #print("client ", current_client, " start")
+            start_time = time.time()
+            federated.work_for_client(current_client, current_client_grad)
+            #print("client", current_client, " complete")
+            end_time = time.time()
+            #print("work for client ", current_client, " cost ", end_time - start_time)
+            if (current_client == args.clients - 1):
+                recover_start = time.time()
+                recovered_grad = federated.recoverGradient()
+                ind = 0
+                recover_end = time.time()
+                #print("recover gradient cost ", recover_end - recover_start)
+                #print(recovered_grad_in_cuda, recovered_grad_in_cuda[0].shape, r)
+                for name, param in model.named_parameters():
+                    if param.requires_grad:
+                        param.grad = recovered_grad[ind]
+                        ind+=1
+                assert(ind == len(recovered_grad))
+                optimizer.step()
+                #print("all clients finished")
+                current_client = 0
+            else :
+                current_client += 1
+
+            # only update the parameters when current_client == args.clients - 1
+
+            # statistics
+            it += 1
+            global_step += 1
+            #running_loss += loss.data[0]
+            running_loss += loss.item()
+            pred = outputs.data.max(1, keepdim=True)[1]
+            if args.mixup:
+                targets = batch['target']
+                targets = Variable(targets, requires_grad=False).cuda(async=True)
+            correct += pred.eq(targets.data.view_as(pred)).sum()
+            total += targets.size(0)
+
+            writer.add_scalar('%s/loss' % phase, loss.item(), global_step)
+
+            # update the progress bar
+            pbar.set_postfix({
+                'loss': "%.05f" % (running_loss / it),
+                'acc': "%.02f%%" % (100*float(correct)/total)
+            })
+            
+            #print("[batch]\t", it, " [loss]\t ", running_loss / it, " [acc] \t", 100 * float(correct)/total)
+            #print('------------------------------------------------------------------')
+            #break
+
+	
+        accuracy = float(correct)/total
+        epoch_loss = running_loss / it
+        writer.add_scalar('%s/accuracy' % phase, 100*accuracy, epoch)
+        writer.add_scalar('%s/epoch_loss' % phase, epoch_loss, epoch)
+    if args.lr_scheduler == 'plateau':
+        lr_scheduler.step(epoch_loss)
+        if (accuracy > best_accuracy):
+            best_accuracy = accuracy
+            checkpoint = {
+                'epoch': epoch,
+                'step': global_step,
+                'state_dict': model.state_dict(),
+                'loss': epoch_loss,
+                'accuracy': accuracy,
+                'optimizer' : optimizer.state_dict(),
+            }
+            torch.save(checkpoint, 'checkpoints/federated-best-loss-speech-commands-checkpoint-%s.pth' % full_name)
+            torch.save(model, '%d-%s-federated-best-loss.pth' % (start_timestamp, full_name))
+            del checkpoint
+    federated.dump()
+if __name__ == '__main__':
+    main()
---
+++ federated_utils_cpu_v3.py
@@ -0,0 +1,283 @@
+import torch, threading
+import time
+import numpy as np
+import math
+import random
+from scipy.linalg import null_space
+#import pandas as pd
+def getLenOfGradientVector(current_grad):
+    #expect a list consists of numpy arrays
+    n = 0
+    for arr in current_grad:
+        #print(list(arr.view(-1,).shape)[0])
+        n += list(arr.view(-1,).shape)[0]
+    return n
+def getShapeList(current_grad):
+    # return the list of shapes of grad vectors, for recover
+    res = []
+    for arr in current_grad:
+        res.append(arr.shape)
+    return res
+def transListOfArraysToArrays(current_grad, n):
+    # expect a list of arrays, return a squeezed array, n is the total length
+    res = np.zeros((n))
+    ind = 0
+    for arr in current_grad:
+        arr = arr.view(-1,).cpu()
+        res[ind:ind+arr.shape[0]] = arr
+        ind+=arr.shape[0]
+    return res
+def listMulti(l):
+    res = 1
+    for ele in l:
+        res *= ele
+    return res
+def transNumpyGrad2Cuda(grad, shape_list):
+    res = []
+    ind = 0
+    for shape in shape_list:
+        tmp = torch.from_numpy(grad[ind:ind+listMulti(shape)])
+        tmp = tmp.view(shape)
+        res.append(tmp.float().cuda())
+        ind += listMulti(shape)
+    return res
+
+############ gpu version
+def getLenOfGradientVectorCuda(current_grad):
+    #expect a list consists of numpy arrays
+    n = 0
+    for arr in current_grad:
+        #print(list(arr.view(-1,).shape)[0])
+        n += list(arr.view(-1,).shape)[0]
+    return n
+def getShapeListCuda(current_grad):
+    # return the list of shapes of grad vectors, for recover
+    res = []
+    for arr in current_grad:
+        res.append(arr.shape)
+    return res
+def get_shape_and_length_gradient_cpu(current_grad):
+    n = 0
+    res = []
+    for arr in current_grad:
+        n += list(arr.view(-1,).shape)[0]
+        res.append(arr.shape)
+    return n, res
+def transListOfArraysToArraysCpu(current_grad, n):
+    # expect a list of arrays, return a squeezed cuda array, n is the total length
+    res = np.zeros((n))
+    ind = 0
+    for arr in current_grad:
+        arr = arr.view(-1,)
+        res[ind:ind+arr.shape[0]] = arr.cpu().numpy()
+        ind+=arr.shape[0]
+    return res
+def trans2numpyArrayWithShapeList(grad, shape_list):
+    res = []
+    ind = 0
+    for shape in shape_list:
+        tmp = grad[ind:ind+listMulti(shape)]
+        res.append(torch.from_numpy(tmp.reshape(shape)).float().cuda())
+        ind += listMulti(shape)
+    return res
+
+class Federated:
+    def __init__(self, num_clients, matrix_size, num_threads, output_path):
+        # utilize a sample gradient vector 
+        self.num_clients = num_clients
+        self.matrix_size = matrix_size
+        self.num_threads = num_threads
+        self.output_path = output_path
+        self.MAX = 0.001
+        self.S_i = random.sample(range(0, 3 * self.matrix_size), self.matrix_size)
+        self.S_i.sort()
+        self.S_j = random.sample(range(0, 2 * self.matrix_size), self.matrix_size)
+        self.S_j.sort()
+        self.all_index = random.sample(range(0, 3 * self.matrix_size), 3 * self.matrix_size)
+        self.rand_index = list(set(self.all_index) - set(self.S_i))
+        # dump indexing matrix
+        np.save(self.output_path + 'S_i.npy', self.S_i)
+        np.save(self.output_path + 'S_j.npy', self.S_j)
+        #TODO
+        self.all_gradient_var = []
+        self.all_gradient_mean = []
+        self.real_gradient_var = []
+        self.real_gradient_mean = []
+        self.rand_gradient_var = []
+        self.rand_gradient_mean = []
+        self.ns_var = []
+        self.ns_var_var = []
+        self.ns_mean = []
+    def init(self, gradient,shape_list):
+        self.len_gradient = list(gradient.shape)[0]
+        self.shape_list = shape_list
+        self.len_gradient_after_padding = math.ceil(float(self.len_gradient) / (self.matrix_size * self.num_threads)) * self.matrix_size * self.num_threads
+        #
+        
+        self.A = self.MAX * np.random.rand(self.matrix_size, self.matrix_size)
+        self.A_inv = np.linalg.inv(self.A)
+        #self.B = np.zeros((self.matrix_size, 3 * self.matrix_size))
+        self.B = np.random.rand(self.matrix_size, 3 * self.matrix_size) * self.MAX
+        for i in range(0, self.matrix_size):
+            self.B[:, self.S_i[i] : self.S_i[i]+1] = self.A[:, i:i+1]
+        self.C = np.random.rand(2 * self.matrix_size, 3 * self.matrix_size) * self.MAX
+        for i in range(0, self.matrix_size):
+            self.C[self.S_j[i] : self.S_j[i] + 1, :] = self.B[i:i+1 , :]
+        
+        # SVD
+        self.u, self.s, self.vh = np.linalg.svd(self.C, full_matrices = True)
+        #self.vh_t = np.transpose(self.vh) numpy doesn't need transpose for reconstruction!
+        self.sigma = np.zeros((self.C.shape[0], self.C.shape[1]))
+        self.sigma[: self.s.shape[0], : self.s.shape[0]] = np.diag(self.s)
+        # null space
+        self.u_sigma = np.dot(self.u, self.sigma)
+        self.ns = null_space(self.C) # (3000, 1000) we use the first args.
+        self.ns_mean.append(np.mean(self.ns))
+        self.ns_var.append(np.mean(np.var(self.ns, axis = 0)))
+        self.ns_var_var.append(np.var(np.var(self.ns, axis = 0)))
+        self.trans_i = np.zeros((self.matrix_size, 3*self.matrix_size))
+        self.trans_j = np.zeros((self.matrix_size, 2*self.matrix_size))
+        for i,ind in  enumerate(self.S_i):
+            self.trans_i[i][ind] = 1
+        for i,ind in enumerate(self.S_j):
+            self.trans_j[i][ind] = 1
+        
+            
+        self.ori_gradient_sum = np.zeros((self.len_gradient))
+        self.random_gradient_sum = np.zeros((self.len_gradient_after_padding * 3))
+#        print("Initialization complete")
+    def work_for_client(self, client_no, gradient):
+        #print("Work for", client_no)
+        part_num = self.len_gradient_after_padding / self.num_threads
+        time1 = time.time()
+        assert(client_no < self.num_clients)
+        flatterned_grad = gradient.cpu().numpy()
+        #flatterned_grad = transListOfArraysToArraysCpu(gradient, self.len_gradient)
+        #call variance of real gradient
+        self.real_gradient_mean.append(np.mean(flatterned_grad))
+        self.real_gradient_var.append(np.var(flatterned_grad))
+        
+        self.ori_gradient_sum += flatterned_grad
+        # padding
+        flatterned_grad_extended = np.zeros((self.len_gradient_after_padding, 1)) # For zero padding
+        flatterned_grad_extended[:self.len_gradient, 0] = flatterned_grad
+        kernel_space = np.zeros((3 * self.matrix_size, 1))
+        random_numbers = self.MAX * np.random.rand(self.matrix_size, 1)
+
+        for i in range(self.matrix_size):
+            kernel_space += random_numbers[i] * self.ns[:, i:i+1]
+        
+        flatterned_grad_extended_after_random = self.MAX * np.random.rand(3 * self.len_gradient_after_padding, 1)
+        ## TODO construct a transformation matrix, replace the assignment with matrix production
+        def randomizing_matrix(thread_id, part_num):
+            for i in range(int(thread_id * part_num), int((thread_id + 1) * part_num), self.matrix_size):
+                #if (thread_id == 0 and int(i/self.matrix_size) % 50 == 0):
+                #    print(thread_id, i/self.matrix_size, (int((thread_id + 1) * part_num)/self.matrix_size))
+                #for j in range(0, self.matrix_size):
+                #    flatterned_grad_extended_after_random[i * 3 + self.S_i[j]] = flatterned_grad_extended[i + j]
+                flatterned_grad_extended_after_random[i * 3 : 3 * (i+self.matrix_size)] = \
+                    (np.dot(flatterned_grad_extended[i : i + self.matrix_size].reshape(1, self.matrix_size), \
+                        self.trans_i)).reshape(3*self.matrix_size,1)
+        threads = []
+        for _i in range(self.num_threads):
+            t = threading.Thread(target = randomizing_matrix, args = (_i, part_num))
+            threads.append(t)
+            t.start()
+        for thread in threads:
+            thread.join()
+        # for i in range(0, self.len_gradient_after_padding, self.matrix_size):
+        #     for j in range(0, self.matrix_size):
+        #         flatterned_grad_extended_after_random[i * 3 + self.S_i[j]] = flatterned_grad_extended[i + j]
+        
+        # compute result
+        time2 = time.time()
+        #print("client ", client_no, " randomization complete")
+        flatterned_grad_extended_final = self.MAX * np.random.rand(3 * self.len_gradient_after_padding, 1)
+        ##TODO: optimize matrix calculation
+        '''
+        for i in range(0, self.len_gradient_after_padding, self.matrix_size):
+            for j in range(0, self.matrix_size):
+                flatterned_grad_extended_final[3 * i : 3*(i + self.matrix_size), :] = np.dot(np.transpose(self.vh), flatterned_grad_extended_after_random[3 * i : 3*(i + self.matrix_size), :] + kernel_space)
+        '''
+        def matrixProd(thread_id, part_num):
+            for i in range(int(thread_id * part_num), int((thread_id + 1) * part_num), self.matrix_size):
+                flatterned_grad_extended_final[3 * i : 3*(i + self.matrix_size), :] \
+                    = np.dot(self.vh, flatterned_grad_extended_after_random[3 * i : 3*(i + self.matrix_size), :] + kernel_space)
+            #print(thread_id, " finish")
+        threads = []
+        for _i in range(self.num_threads):
+            t = threading.Thread(target = matrixProd, args = (_i, part_num))
+            threads.append(t)
+            t.start()
+        for thread in threads:
+            thread.join()
+        self.random_gradient_sum += flatterned_grad_extended_final[:, 0]
+        self.all_gradient_mean.append(np.mean(flatterned_grad_extended_final[:, 0]))
+        self.all_gradient_var.append(np.var(flatterned_grad_extended_final[:, 0]))
+
+#        self.rand_gradient_mean.append((flatterned_grad_extended_final[:,0].shape[0] * self.all_gradient_mean[-1] - flatterned_grad.shape[0] * self.real_gradient_mean[-1])/ ( flatterned_grad_extended_final[:,0].shape[0] -  flatterned_grad.shape[0]))
+        self.rand_gradient_mean.append((np.sum(flatterned_grad_extended_final) - np.sum(flatterned_grad))/( flatterned_grad_extended_final[:,0].shape[0] -  flatterned_grad.shape[0])) 
+        # calculating the average of randomized gradient
+        #mean = 0
+        #for i in range(0, 3 * self.len_gradient_after_padding, 3 * self.matrix_size):
+        #    for index in self.rand_index:
+        #        mean += flatterned_grad_extended_final[i + index][0]
+        #print(mean)
+        #mean = mean / ( 3 * self.len_gradient_after_padding - self.len_gradient)
+        #print('random mean', mean, self.rand_gradient_mean[-1])
+#        print(3 * self.len_gradient_after_padding - self.len_gradient ,flatterned_grad_extended_final[:,0].shape[0] -  flatterned_grad.shape[0])
+        rand_gradient_var = 0
+        for i in range(0, 3 * self.len_gradient_after_padding, 3 * self.matrix_size):
+            for index in self.rand_index:
+                rand_gradient_var += (flatterned_grad_extended_final[index][0] - self.rand_gradient_mean[-1])**2
+        rand_gradient_var = rand_gradient_var / (3 * self.len_gradient_after_padding - self.len_gradient)
+        self.rand_gradient_var.append(rand_gradient_var)
+        #print(self.rand_gradient_var[-1], self.real_gradient_var[-1], self.all_gradient_var[-1])
+        #print("client ",  client_no, " masking complete",)
+        #print("time for randomization ", time2 - time1, "time for masking", time3 - time2)
+    def recoverGradient(self):
+        time1 = time.time()
+        res = np.zeros((self.len_gradient_after_padding, 1))
+        alpha = np.zeros((self.matrix_size, 1))
+        
+        for i in range(0, self.len_gradient_after_padding * 3 , 3 * self.matrix_size):
+            tmp = np.dot(self.u_sigma, self.random_gradient_sum[i : i + 3 * self.matrix_size]) # (2n,1)
+            alpha = np.dot(self.trans_j, tmp)
+            alpha = np.reshape(alpha, (self.matrix_size,1))
+            #for j in range(self.matrix_size): 
+            #    alpha[j] = tmp[self.S_j[j]]
+            res[int(i/3) : int(i/3) + self.matrix_size] = np.dot(self.A_inv, alpha)
+        # set the gradient manually and update
+        recovered_grad_in_list = trans2numpyArrayWithShapeList(res, self.shape_list)
+        time2 = time.time()
+        #print('[dist]\t', np.sum(np.abs(res[:self.len_gradient, 0] - self.ori_gradient_sum)))
+        #print('ori ', self.ori_gradient_sum[:10])
+        #print("rec ", res[:10])
+        #print("Recover gradient cost ", time2 - time1)
+        return recovered_grad_in_list
+    def writetxt(self, filename, l):
+        f = open(filename, 'w')
+        f.write('\n'.join(l))
+        f.close()
+    def dump(self):
+        '''
+        np.save(self.output_path + './all_gradient_mean.npy', np.array(self.all_gradient_mean))
+        np.save(self.output_path + './all_gradient_var.npy', np.array(self.all_gradient_var))
+        np.save(self.output_path +  np.array(self.rand_gradient_mean))
+        np.save(self.output_path + './rand_gradient_var.npy', np.array(self.rand_gradient_var))
+        np.save(self.output_path + './real_gradient_mean.npy', np.array(self.real_gradient_mean))
+        np.save(self.output_path + './real_gradient_var.npy', np.array(self.real_gradient_var))
+        np.save(self.output_path + './kernel_mean.npy', np.array(self.ns_mean))
+        np.save(self.output_path + './kernel_var.npy', np.array(self.ns_var))
+        np.save(self.output_path + './kernel_var_var.npy', np.array(self.ns_var_var))
+        ''' 
+        self.writetxt(self.output_path + './all_gradient_mean.txt', self.all_gradient_mean)
+        self.writetxt(self.output_path + ' ./all_gradient_var.txt', self.all_gradient_var)
+        self.writetxt(self.output_path + './rand_gradient_mean.txt', self.rand_gradient_mean)
+        self.writetxt(self.output_path + './rand_gradient_var.txt', self.rand_gradient_var)
+        self.writetxt(self.output_path + './real_gradient_mean.txt',self.real_gradient_mean)
+        self.writetxt(self.output_path + './real_gradient_var.txt', self.real_gradient_var)
+        self.writetxt(self.output_path + './kernel_mean.txt', self.ns_mean)
+        self.writetxt(self.output_path + './kernel_var.txt', self.ns_var)
+        self.writetxt(self.output_path + './kernel_var_var.txt', self.ns_var_var)
+        print("successfully dumped")
